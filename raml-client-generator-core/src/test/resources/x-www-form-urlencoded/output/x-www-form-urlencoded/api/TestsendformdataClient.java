555e704b686a9f366664b84457821fc3563a3044
mzko <mzko@tut.by>
2016-06-16 13:08:08 +0300

    Add x-www-form-urlencoded support

---
+++ raml-client-generator-core/src/test/resources/x-www-form-urlencoded/output/x-www-form-urlencoded/api/TestsendformdataClient.java
@@ -0,0 +1,35 @@
+
+package x-www-form-urlencoded.api;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import x-www-form-urlencoded.resource.sendFormData.SendFormData;
+
+
+/**
+ * Some content
+ * 
+ */
+public class TestsendformdataClient {
+
+    private String _baseUrl;
+    public final SendFormData sendFormData;
+
+    public TestsendformdataClient(String baseUrl) {
+        _baseUrl = baseUrl;
+        sendFormData = new SendFormData(getBaseUri(), getClient());
+    }
+
+    private Client getClient() {
+        return ClientBuilder.newClient();
+    }
+
+    protected String getBaseUri() {
+        return _baseUrl;
+    }
+
+    public static TestsendformdataClient create(String baseUrl) {
+        return new TestsendformdataClient(baseUrl);
+    }
+
+}
---
+++ raml-client-generator-core/src/test/resources/x-www-form-urlencoded/output/x-www-form-urlencoded/resource/sendFormData/SendFormData.java
@@ -0,0 +1,48 @@
+
+package x-www-form-urlencoded.resource.sendFormData;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status.Family;
+import x-www-form-urlencoded.resource.sendFormData.model.SendFormDataPOSTBody;
+
+public class SendFormData {
+
+    private String _baseUrl;
+    private Client client;
+
+    public SendFormData(String baseUrl, Client client) {
+        _baseUrl = (baseUrl +"/sendFormData");
+        this.client = client;
+    }
+
+    private Client getClient() {
+        return this.client;
+    }
+
+    private String getBaseUri() {
+        return _baseUrl;
+    }
+
+    public void post(SendFormDataPOSTBody body) {
+        WebTarget target = this.client.target(getBaseUri());
+        final javax.ws.rs.client.Invocation.Builder invocationBuilder = target.request(javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE);
+        MultivaluedMap multiValuedMap = new MultivaluedHashMap();
+        if (body.getParam1()!= null) {
+            multiValuedMap.add("param1", body.getParam1().toString());
+        }
+        if (body.getParam2()!= null) {
+            multiValuedMap.add("param2", body.getParam2().toString());
+        }
+        Response response = invocationBuilder.post(Entity.entity(multiValuedMap, javax.ws.rs.core.MediaType.APPLICATION_FORM_URLENCODED_TYPE));
+        if (response.getStatusInfo().getFamily()!= Family.SUCCESSFUL) {
+            Response.StatusType statusInfo = response.getStatusInfo();
+            throw new RuntimeException(((((("("+ statusInfo.getFamily())+") ")+ statusInfo.getStatusCode())+" ")+ statusInfo.getReasonPhrase()));
+        }
+    }
+
+}
