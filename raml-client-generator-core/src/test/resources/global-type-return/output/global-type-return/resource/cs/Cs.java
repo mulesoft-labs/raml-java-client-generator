baa939dae8207975176fcc6362168840ce2d9b8b
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2017-06-17 10:38:17 +0200

    Allow using global types as return types
    Currently referencing a global type as return type results in a generated
    Java method which returns type "String". Change the code so it handles
    this, just the code that handles the body (Java method parameter) does.
    Also add a separate test for this to ensure that this doesn't break in the
    future.

---
+++ raml-client-generator-core/src/test/resources/global-type-return/output/global-type-return/resource/cs/Cs.java
@@ -0,0 +1,27 @@
+
+package global-type-return.resource.cs;
+
+import javax.ws.rs.client.Client;
+import global-type-return.resource.cs.login.Login;
+
+public class Cs {
+
+    private String _baseUrl;
+    private Client client;
+    public final Login login;
+
+    public Cs(String baseUrl, Client client) {
+        _baseUrl = (baseUrl +"/cs");
+        this.client = client;
+        login = new Login(getBaseUri(), getClient());
+    }
+
+    private Client getClient() {
+        return this.client;
+    }
+
+    private String getBaseUri() {
+        return _baseUrl;
+    }
+
+}
